e55701f931dfaef779cc4a6f7ee006ed725762be
matias <matiasezequielgonzalez365@gmail.com>
2026-09-01 11:24:13 -0300

    feat: add Product, fix Category and generate database migrations

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 from django.utils.text import slugify
 
@@ -15,11 +16,11 @@ class Category(models.Model):
         related_name="subcategories"
     )
     
-    active = models.DateTimeField(auto_now_add=True)
+    created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     
     class Meta:
-        verbose_name = "Categorie"
+        verbose_name = "Category"
         verbose_name_plural = "Categories"
         ordering = ["name"]
         
@@ -31,3 +32,18 @@ class Category(models.Model):
         if not self.slug:
             self.slug = slugify(self.name)
         super().save(*args, **kwargs)
+        
+
+class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
+    name = models.CharField(max_length=250)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=10)
+    image = models.ImageField( upload_to="apps.products/", blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
