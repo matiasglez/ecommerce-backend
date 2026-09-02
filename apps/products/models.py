fb955a646631c9f1985be147dd21e98ce9f9721d
matias <matiasezequielgonzalez365@gmail.com>
2026-09-02 17:19:50 -0300

    update file upload destination and apply migrations

--- apps/products/models.py
+++ apps/products/models.py
@@ -41,7 +41,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField(default=10)
-    image = models.ImageField( upload_to="media.products/", blank=True, null=True)
+    image = models.ImageField( upload_to="products/", blank=True, null=True)
     is_active = models.BooleanField(default=True)
     created_on = models.DateTimeField(auto_now_add=True)
     
