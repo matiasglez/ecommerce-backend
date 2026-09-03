de86d61c017ad814f72d8a0ed3d0bb40951cf6e4
matias <matiasezequielgonzalez365@gmail.com>
2026-09-03 17:16:05 -0300

    feat(cart): add cart and cart item models

--- apps/cart/models.py
+++ apps/cart/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.conf import settings
+from apps.products.models import Product
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+class Cart(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="cart")
+    created_on = models.DateTimeField(auto_now_add=True)
+    
+    @property
+    def total(self):
+        return sum(item.subtotal for item in self.items.all())
+    
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1)]) # Explicito quantity >= 1
+    
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["cart", "product"],
+                name="unique_product_per_cart",
+            )
+        ]
+        
+    @property
+    def subtotal(self):
+        return self.product.price * self.quantity
