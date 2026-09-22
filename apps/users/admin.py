d2a444e0e20d2302a87c8e239d928f59595c6734
matias <matiasezequielgonzalez365@gmail.com>
2026-09-22 17:18:24 -0300

    create admin interface for users

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from apps.users.models import User 
 
-# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    ordering = ["email"]
+    list_display = ["email", "is_active", "is_staff", "is_superuser"]
+    list_filter = ["is_active", "is_staff", "is_superuser"]
+    search_fields = ["email"]
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "password1", "password2", "is_active", "is_staff", "is_superuser"),
+        }),
+    )
+    filter_horizontal = ("groups", "user_permissions")
+    list_per_page = 20
